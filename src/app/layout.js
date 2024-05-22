8c5afd57de4f26f00e170262e6c5eb1940926d40
Will <145696026+will-bbg@users.noreply.github.com>
2024-05-21 21:14:43 -0500

    feature: got custom bootstrap variables working

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,7 @@ import { Inter } from "next/font/google";
 import Header from "../app/components/Header";
 import Footer from "../app/components/Footer";
 import BootstrapClient from '../app/components/BootstrapClient.js';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../app/styles/globals.scss";
+import "../app/scss/main.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 import Header from "../app/components/Header";
 import Footer from "../app/components/Footer";
 import BootstrapClient from '../app/components/BootstrapClient';
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "../app/styles/globals.scss";
+import "../app/scss/main.scss";
+
 import SmoothScrolling from "../app/components/SmoothScrolling";
 
 export default function MyApp({ Component, pageProps }) {

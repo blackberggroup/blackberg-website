9ad25672b66410bd116764260a0d6a3aff394d47
Will <145696026+will-bbg@users.noreply.github.com>
2024-05-22 10:33:19 -0500

    feature: adds custom local fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,14 @@
-import { Inter } from "next/font/google";
 import Header from "../app/components/Header";
 import Footer from "../app/components/Footer";
 import BootstrapClient from '../app/components/BootstrapClient.js';
 import "../app/scss/main.scss";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         <main>{children}</main>
         <Footer />

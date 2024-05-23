8ac72669dddedea2dd98903fb3a2b133dc72b919
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-05-23 15:58:10 -0700

    bugfix: move nav logic to client side; fix link structure

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,13 +1,8 @@
 
 import Link from 'next/link';
 import React from 'react';
-import { usePathname } from 'next/navigation';
 
 const Header = ({ nav }) => {
-
-    const pathname = usePathname();
-    const currentPath = pathname.split('/').pop();
-    console.log('Current Path', currentPath);
 
     return (
         <header className="bg-primary">
@@ -23,9 +18,8 @@ const Header = ({ nav }) => {
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <Link href="/" 
-                                    className={`nav-link ${currentPath === '' ? 'active' : ''}`} 
-                                    aria-label="Home page"
-                                    aria-current={`${currentPath === '' ? 'page' : 'false'}`}>
+                                    className="nav-link"
+                                    aria-label="Home page">
                                     Home
                                 </Link>
                             </li>
@@ -36,23 +30,21 @@ const Header = ({ nav }) => {
                                 return (
                                 <li key={index} className="nav-item dropdown">
                                     <Link 
-                                        className={`nav-link dropdown-toggle ${currentPath === item.url ? 'active' : ''}`} 
+                                        className="nav-link dropdown-toggle"
                                         href={`/${item.url}`} 
                                         id={`navbarDropdown-${index}`} 
                                         role="button" 
                                         data-bs-toggle="dropdown" 
-                                        aria-expanded="false"
-                                        aria-current={`${currentPath === item.url ? 'page' : 'false'}`}>
+                                        aria-expanded="false">
                                         {item.displayText}
                                     </Link>
                                     <ul className="dropdown-menu" aria-labelledby={`navbarDropdown-${index}`}>
                                         {item.page.map((subItem, subIndex) => (
                                             <li key={subIndex}>
                                                 <Link 
-                                                    href={`${item.url}/${subItem.slug}`} 
-                                                    className={`dropdown-item ${currentPath === subItem.slug ? 'active' : ''}`} 
-                                                    aria-label={`${subItem.title} page`}
-                                                    aria-current={`${currentPath === subItem.slug ? 'page' : 'false'}`}>
+                                                    href={`/${item.url}/${subItem.slug}`} 
+                                                    className="dropdown-item"
+                                                    aria-label={`${subItem.title} page`}>
                                                     {subItem.title}
                                                 </Link>
                                             </li>
@@ -65,9 +57,8 @@ const Header = ({ nav }) => {
                                 <li key={index} className="nav-item">
                                     <Link 
                                         href={`/${item.page[0].slug}`} 
-                                        className={`nav-link ${currentPath === item.page[0].slug ? 'active' : ''}`} 
-                                        aria-label={`${item.displayText} page`}
-                                        aria-current={`${currentPath === item.page[0].slug ? 'page' : 'false'}`}>
+                                        className="nav-link"
+                                        aria-label={`${item.displayText} page`}>
                                         {item.displayText}
                                     </Link>
                                 </li>
--- src/app/components/HeaderInteractive.js
+++ src/app/components/HeaderInteractive.js
@@ -8,18 +8,36 @@ const NavInteractivity = () => {
 
   useEffect(() => {
     const navItems = document.querySelectorAll('.navbar .nav-item');
+    const navLinks = document.querySelectorAll('.navbar .nav-link, .navbar .dropdown-item');
 
     const handleClick = (e) => {
       const elLink = e.currentTarget.querySelector('a[data-bs-toggle]');
       if (elLink && !e.target.closest('.dropdown-menu')) {
         e.preventDefault(); // Prevent default link behavior
-        router.push(elLink.href); // Use Next.js router to navigate
+        router.replace(elLink.href); // Use Next.js router to navigate
       }
     };
 
     navItems.forEach((item) => {
       item.addEventListener('click', handleClick);
     });
+
+    const setActiveLink = () => {
+        const currentPath = router.asPath.split('/').pop();
+
+        navLinks.forEach((item) => {
+        const itemPath = item.getAttribute('href').split('/').pop();
+        if (itemPath === currentPath) {
+            item.classList.add('active');
+            item.setAttribute('aria-current', 'page');
+        } else {
+            item.classList.remove('active');
+            item.removeAttribute('aria-current');
+        }
+        });
+    };
+
+    setActiveLink();
 
     // Cleanup event listeners on component unmount
     return () => {
@@ -33,3 +51,72 @@ const NavInteractivity = () => {
 };
 
 export default NavInteractivity;
+// 'use client';
+
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+
+// const NavInteractivity = () => {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const navItems = document.querySelectorAll('.navbar .nav-link, .navbar .dropdown-item');
+
+//     const setActiveLink = () => {
+//       const currentPath = router.asPath.split('/').pop();
+
+//       navItems.forEach((item) => {
+//         const itemPath = item.getAttribute('href').split('/').pop();
+//         if (itemPath === currentPath) {
+//           item.classList.add('active');
+//           item.setAttribute('aria-current', 'page');
+//         } else {
+//           item.classList.remove('active');
+//           item.removeAttribute('aria-current');
+//         }
+//       });
+//     };
+
+//     const handleClick = (e) => {
+//       const elLink = e.currentTarget.querySelector('a[data-bs-toggle]');
+//       if (elLink && !e.target.closest('.dropdown-menu')) {
+//         e.preventDefault(); // Prevent default link behavior
+//         router.push(elLink.href); // Use Next.js router to navigate
+//       }
+//     };
+
+//     const handleDropdownClick = (e) => {
+//       const target = e.target.closest('a[data-bs-toggle]');
+//       if (target && target.href && !e.target.closest('.dropdown-menu')) {
+//         e.preventDefault(); // Prevent default link behavior
+//         router.push(target.href); // Use Next.js router to navigate
+//       }
+//     };
+
+//     setActiveLink();
+//     navItems.forEach((item) => {
+//       item.addEventListener('click', handleClick);
+//     });
+
+//     document.querySelectorAll('.navbar .dropdown-toggle').forEach((item) => {
+//       item.addEventListener('click', handleDropdownClick);
+//     });
+
+//     router.events.on('routeChangeComplete', setActiveLink);
+
+//     // Cleanup event listeners on component unmount
+//     return () => {
+//       navItems.forEach((item) => {
+//         item.removeEventListener('click', handleClick);
+//       });
+//       document.querySelectorAll('.navbar .dropdown-toggle').forEach((item) => {
+//         item.removeEventListener('click', handleDropdownClick);
+//       });
+//       router.events.off('routeChangeComplete', setActiveLink);
+//     };
+//   }, [router]);
+
+//   return null; // This component does not render anything
+// };
+
+// export default NavInteractivity;

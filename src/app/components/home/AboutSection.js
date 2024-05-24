6ac9f1e75ccd05ed641f2aa967d2cb6dacbfdc94
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-05-24 14:11:46 -0700

    feat: adjust styling and animation

--- src/app/components/home/AboutSection.js
+++ src/app/components/home/AboutSection.js
@@ -14,7 +14,7 @@ const AboutSection = () => {
 
         // Scale animation
         gsap.fromTo(element, 
-            { scale: 0.75 }, 
+            { scale: 0.5 }, 
             {
             scale: 1,
             duration: 2,

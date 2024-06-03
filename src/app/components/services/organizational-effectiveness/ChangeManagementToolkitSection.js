19d8c6c254a5892589bb7bd2723f015090178d92
Will <145696026+will-bbg@users.noreply.github.com>
2024-06-03 16:08:10 -0500

    bug: character fix

--- src/app/components/services/organizational-effectiveness/ChangeManagementToolkitSection.js
+++ src/app/components/services/organizational-effectiveness/ChangeManagementToolkitSection.js
@@ -56,7 +56,7 @@ const ChangeManagementToolkitSection = () => {
                                                         <path stroke="#101828" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h-.2c-1.68 0-2.52 0-3.162-.327a3 3 0 0 1-1.311-1.311C12 17.72 12 16.88 12 15.2V8.8c0-1.68 0-2.52.327-3.162a3 3 0 0 1 1.311-1.311C14.28 4 15.12 4 16.8 4h.2m0 16a2 2 0 1 0 4 0 2 2 0 0 0-4 0Zm0-16a2 2 0 1 0 4 0 2 2 0 0 0-4 0ZM7 12h10M7 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm10 0a2 2 0 1 0 4 0 2 2 0 0 0-4 0Z"/>
                                                     </svg>
                                                 </div>
-                                                <h3 className="h5 m-0">Kotter's Model</h3>
+                                                <h3 className="h5 m-0">Kotter&lsquo;s Model</h3>
                                             </div>                                            
                                         </div>
                                     </div>

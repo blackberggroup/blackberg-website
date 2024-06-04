cd6678ca617458572dfb1242e6a4118bbef6f091
Will <145696026+will-bbg@users.noreply.github.com>
2024-06-03 22:54:23 -0500

    bug: character fix

--- src/app/components/services/communications/IntroSection.js
+++ src/app/components/services/communications/IntroSection.js
@@ -8,7 +8,7 @@ const IntroSection = () => {
                     <div className="row align-items-center">
                         <div className="col-12 col-lg-7">
                             <p className="lead">
-                                In the digital age, the power of effective communication is unparalleled in shaping an organization's success. At Blackberg, our communications experts are dedicated to mastering this art, ensuring your message not only reaches but also resonates with your audience. Communication is the lifeline of your brand&mdash;it defines your identity, conveys your values, and builds lasting relationships with your stakeholders. We are committed to not just delivering your message, but also enhancing your narrative, fostering engagement, and driving growth to leave a lasting impression.
+                                In the digital age, the power of effective communication is unparalleled in shaping an organization&lsquo;s success. At Blackberg, our communications experts are dedicated to mastering this art, ensuring your message not only reaches but also resonates with your audience. Communication is the lifeline of your brand&mdash;it defines your identity, conveys your values, and builds lasting relationships with your stakeholders. We are committed to not just delivering your message, but also enhancing your narrative, fostering engagement, and driving growth to leave a lasting impression.
                             </p>
                         </div>
                     </div>

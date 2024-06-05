148d6b923eb8c78dd9b59d71b05e2c9a4af4eed8
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-06-05 09:33:43 -0700

    feat: updating hygraph integration

---
+++ src/app/components/case-studies/dynamic/ResultsSection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+
+const ResultsSection = ({ page }) => {
+
+    return (
+        <section id="results" className="py-8 py-md-11" aria-label="Results">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-8 col-lg-6 mx-auto">
+                        <h2 className="display-5 mb-3">The Result</h2>
+                        <p className="m-0 mb-5">{page.resultContent}</p>
+                        <h3>Share this case study:</h3>
+                        <div className="social-share mt-4">
+                            <Link href="" target="_blank" className="btn btn-outline" aria-label="Share VA Immersive National Marketing case study on LinkedIn">LinkedIn</Link>
+                            <Link href="" target="_blank" className="btn btn-outline" aria-label="Share VA Immersive National Marketing case study on Twitter">Twitter</Link>
+                            <Link href="" target="_blank" className="btn btn-outline" aria-label="Share VA Immersive National Marketing case study on Facebook">Facebook</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ResultsSection;

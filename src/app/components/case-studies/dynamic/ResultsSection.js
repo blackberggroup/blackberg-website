6009342c1bf12965638e27dd293261559f687465
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-06-11 09:59:33 -0700

    feat: update social share link; add the solution content;

--- src/app/components/case-studies/dynamic/ResultsSection.js
+++ src/app/components/case-studies/dynamic/ResultsSection.js
@@ -12,9 +12,9 @@ const ResultsSection = ({ page }) => {
                         <p className="m-0 mb-5">{page.resultContent}</p>
                         <h3>Share this case study:</h3>
                         <div className="social-share mt-4">
-                            <Link href="" target="_blank" className="btn btn-outline" aria-label="Share VA Immersive National Marketing case study on LinkedIn">LinkedIn</Link>
-                            <Link href="" target="_blank" className="btn btn-outline" aria-label="Share VA Immersive National Marketing case study on Twitter">Twitter</Link>
-                            <Link href="" target="_blank" className="btn btn-outline" aria-label="Share VA Immersive National Marketing case study on Facebook">Facebook</Link>
+                            <Link href={`https://www.linkedin.com/sharing/share-offsite/?url=${page.slug}`} target="_blank" className="btn btn-outline" aria-label={`Share ${page.title} case study on LinkedIn`}>LinkedIn</Link>
+                            <Link href={`https://www.twitter.com/intent/tweet?text=${page.title}&via=blackberggroup&url=https://blackberggroup.com/case-studies/${page.slug}`} target="_blank" className="btn btn-outline" aria-label={`Share ${page.title} case study on Twitter`}>Twitter</Link>
+                            <Link href={`https://www.facebook.com/sharer/sharer.php?u=https://blackberggroup.com/case-studies/${page.slug}`} target="_blank" className="btn btn-outline" aria-label={`Share ${page.title} case study on Facebook`}>Facebook</Link>
                         </div>
                     </div>
                 </div>

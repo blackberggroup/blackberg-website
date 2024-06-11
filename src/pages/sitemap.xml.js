b5692a8e4f7f7888e68f197cab445fc1313b15c8
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-06-11 06:51:51 -0700

    feat: update base url

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -3,16 +3,14 @@ import { getAllPagePaths } from '../app/lib/hygraph';
 import { getServerSideSitemapLegacy } from 'next-sitemap';
 
 export const getServerSideProps = async (ctx) => {
+  
   const paths = await getAllPagePaths();
-  console.log("Paths: ", paths);
 
   const fields = paths.map((path) => ({
-    loc: `https://testsite.com${path}`,
+    loc: `https://blackberggroup.com${path}`,
     changefreq: 'daily',
     priority: 1,
   }));
-
-  console.log("Fields:", fields); // Debug log
 
   return getServerSideSitemapLegacy(ctx, fields);
 };

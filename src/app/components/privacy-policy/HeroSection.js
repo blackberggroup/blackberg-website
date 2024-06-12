04eab7de01b57fad48f45818f5405d3df4b460dd
Will <145696026+will-bbg@users.noreply.github.com>
2024-06-12 12:50:50 -0500

    feat: adds privacy policy page

---
+++ src/app/components/privacy-policy/HeroSection.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+const HeroSection = () => {
+
+    return (
+        <section id="privacy-policy-hero" className="hero hero--privacy-policy">
+            <div className="container h-100">
+                <div className="row align-items-center h-100">
+                    <div className="col-12 col-md-11 col-lg-8 col-xl-7 text-center mx-auto">
+                        <div className="hero-title">
+                            <span className="text-headline-label text-headline-label--secondary text-uppercase">Blackberg Group</span>
+                            <h1 className="display-5 mb-3 mb-lg-5">Privacy Policy</h1>
+                        </div>                            
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeroSection;

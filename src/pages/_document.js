a1bb5dfc885d90ad0d97abd582e5b5db4d30a1d0
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-06-12 11:45:38 -0700

    feat: setting up gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','G-635LJGHMSL');`,
+      })(window,document,'script','dataLayer','GTM-TJH68ZFB');`,
     }}
   />
         </Head>
@@ -21,7 +21,7 @@ class MyDocument extends Document {
             <NextScript />
             <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-635LJGHMSL" height="0" width="0" style="display: none; visibility: hidden;" />`,
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJH68ZFB" height="0" width="0" style="display: none; visibility: hidden;" />`,
             }}
           />
         </body>

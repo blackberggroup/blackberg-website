85fb06e0006a589305dd4a2c333c9ae1070aa7d1
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-06-17 13:11:39 -0700

    feat: make env variable more descriptive

--- src/pages/api/robots.js
+++ src/pages/api/robots.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    const environment = process.env.ENV;
+    const environment = process.env.VERCEL_ENV;
     console.log('Environment: ' + environment);
 
     let robotsTxt = '';

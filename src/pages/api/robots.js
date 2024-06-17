808b5cd7f8e782311cff077219a12999f401db46
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-06-17 11:59:18 -0700

    feat: setup logic for environment based robots file

---
+++ src/pages/api/robots.js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+    const environment = process.env.NEXT_PUBLIC_ENV;
+
+    let robotsTxt = '';
+
+    if (environment === 'production') {
+        robotsTxt = `User-agent: *
+Disallow: 
+
+Sitemap: https://blackberggroup.com/sitemap.xml
+        `;
+    } else {
+        robotsTxt = `User-agent: *
+Disallow: /
+        `;
+    }
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.write(robotsTxt);
+    res.end();
+}

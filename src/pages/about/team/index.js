86c7918a213b974c19c317ed841a7149a37c6a7b
rb-blackberg <ross.burmeister@blackberggroup.com>
2024-08-13 12:04:50 -0700

    feat: add job openings CTA

--- src/pages/about/team/index.js
+++ src/pages/about/team/index.js
@@ -1,6 +1,6 @@
 import SEOHead from '@/app/components/SEOHead';
-import JobOpeningsSection from '@/app/components/about/JobOpeningsSection';
 import HeroSection from '@/app/components/about/team/HeroSection';
+import JobOpeningsSection from '@/app/components/about/team/JobOpeningsSection';
 import TeamGridSection from '@/app/components/about/team/TeamGridSection';
 import { getAllEmployees, getPageBySlug } from '@/app/lib/hygraph';
 

078b31553d25a87a38f445d09aec2c184373ff33
Will <145696026+will-bbg@users.noreply.github.com>
2025-03-14 11:10:45 -0500

    feat: character fix

--- src/app/components/portfolio/HeroSection.js
+++ src/app/components/portfolio/HeroSection.js
@@ -9,7 +9,7 @@ const HeroSection = () => {
                     <div className="col-12 col-md-12 col-lg-7">
                         <div className="hero-title">
                             <h1 className="display-2 text-white mb-3 mb-lg-5">Portfolio</h1>
-                            <p className="lead text-white">Discover our dynamic portfolio, showcasing a diverse range of branding, event, and publication projects designed to elevate your brand's voice. With deep expertise in communication and marketing, our work highlights creative strategies and successful implementations that drive results and effectively engage audiences.</p>
+                            <p className="lead text-white">Discover our dynamic portfolio, showcasing a diverse range of branding, event, and publication projects designed to elevate your brand&rsquo;s voice. With deep expertise in communication and marketing, our work highlights creative strategies and successful implementations that drive results and effectively engage audiences.</p>
                         </div>                            
                     </div>
                 </div>

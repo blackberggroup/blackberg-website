9c27aa1eaab2541292d16fab6b0d8dbb39308db1
rb-blackberg <ross.burmeister@blackberggroup.com>
2025-07-08 09:15:52 -0600

    fix: hygraph asset migration

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,11 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.graphassets.com',
+        pathname: '/**',
+      },
     ],
   },
   async rewrites() {
